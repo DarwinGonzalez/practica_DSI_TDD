b18ae163e158ce72dd1137ab1b22626d20062699
Darwin Gonzalez Suarez <alu0100812794@ull.edu.es>
2018-04-30 11:00:37 +0000

    Primer caso TDD

---
+++ primer_Caso/app-1.js
@@ -0,0 +1,9 @@
+var assert = require('assert');
+
+// assert.deepEqual(Raices(1,-5,0), [[5,0],[2,0]]); ERROR contenido en erro.log
+
+function Raices(a, b, c) {
+    return [[5,0],[2,0]];
+}
+
+assert.deepEqual(Raices(1,-5,0), [[5,0],[2,0]])
